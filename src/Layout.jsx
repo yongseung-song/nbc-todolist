e3920ad9de05d261b600830a2904b55e296ff69f
Yongseung Song <yongseung.song@icloud.com>
2023-11-03 16:40:57 +0900

    Chore: create jsconfig.json and added path

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout() {
+  const style = {
+    minWidth: "800px",
+    maxWidth: "1200px",
+    margin: "0 auto",
+    border: "1px solid black",
+  };
+  return <div style={style}>리액트 첫 과제 시작!</div>;
+}
+
+export default Layout;
